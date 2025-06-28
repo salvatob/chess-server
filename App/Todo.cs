f21366197475cf1ad348e87211b44b2d3115e1f7
Tobiáš Salva <tobias@avlas.cz>
2025-06-28 23:20:38 +0200

    Initial commit

---
+++ App/Todo.cs
@@ -0,0 +1,9 @@
+namespace App;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+}
---
+++ App/TodoDb.cs
@@ -0,0 +1,10 @@
+namespace App;
+
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext {
+  public TodoDb(DbContextOptions<TodoDb> options)
+    : base(options) { }
+
+  public DbSet<Todo> Todos => Set<Todo>();
+}
