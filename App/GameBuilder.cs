946e3cda92f90680d68008e3dd699e917d23bc1b
salvat <tobias@avlas.cz>
2026-07-28 13:45:02 +0200

    Create and implement a simple game builder class

---
+++ App/GameBuilder.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using ChessBotCore;
+using ChessBotCore.Game;
+
+namespace App;
+
+public class GameBuilder {
+    public IPlayer? WhitePlayer { private get; set; }
+    public IPlayer? BlackPlayer { private get; set; }
+    public bool WhiteIsSet => WhitePlayer != null;
+    public bool BlackIsSet => BlackPlayer != null;
+    public Timers Timers  { private get; set; } =  new Timers();
+    public State State { private get; set; } = State.Initial;
+    
+    public bool Ready => WhiteIsSet && BlackIsSet;
+
+    public ChessGame Build() {
+        if (!Ready) {
+            throw new InvalidOperationException("Players are not set yet.");
+        }
+        return new ChessGame(WhitePlayer!, BlackPlayer!, Timers, State);
+    }
+}
