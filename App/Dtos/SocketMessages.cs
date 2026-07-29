574daf24afee5a115070323574dc5de0fea7c810
salvat <tobias@avlas.cz>
2026-07-29 10:13:07 +0200

    Introduce CreateGameDTO

--- App/Dtos/SocketMessages.cs
+++ App/Dtos/SocketMessages.cs
@@ -33,3 +33,11 @@ public class EndGameDto : SocketMessage {
 public class MoveDtoMessage : SocketMessage {
     public MoveDTO? Move { get; set; }
 }
+
+public class CreateGameDto {
+    public TimeSpan WhiteTime { get; set; }
+    public TimeSpan BlackTime { get; set; }
+    public TimeSpan Increment { get; set; }
+    public string Opponent { get; set; } = "";
+    public string Side { get; set; } = "";
+}
