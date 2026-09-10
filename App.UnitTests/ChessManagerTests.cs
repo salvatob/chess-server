8a65f6770e819863da11141f97ac335bbb0ca2af
salvat <tobias@avlas.cz>
2026-09-10 11:46:31 +0200

    Revrite vibe coded chess manager tests

--- App.UnitTests/ChessManagerTests.cs
+++ App.UnitTests/ChessManagerTests.cs
@@ -1,14 +1,9 @@
-using ChessBotCore;
-using ChessBotCore.Game;
-using ChessBotCore.Players;
-using Xunit;
-
 namespace App.UnitTests;
 
 public class ChessManagerTests {
     private const string MateInOneFen = "4k3/8/4K3/8/8/8/8/4Q3 w - - 0 1";
 
-    private async Task<(int id, MockPlayer white, MockPlayer black)> SetupGame(ChessManager manager, string? fen = null) {
+    private (int id, MockPlayer white, MockPlayer black) SetupGame(ChessManager manager, string? fen = null) {
         var white = new MockPlayer();
         var black = new MockPlayer();
         int id = manager.CreateGame(TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(2), fen);
@@ -26,14 +21,14 @@ public class ChessManagerTests {
         
         // Act
         // Use a FEN that is almost mate so the game ends quickly
-        var (id, white, black) = await SetupGame(manager, MateInOneFen);
+        var (id, white, black) = SetupGame(manager, MateInOneFen);
 
         manager.StartGame(id);
         
         // Assert
         // The game should finish very quickly because the MockPlayer will play the winning move
         // or just some move that leads to end. 
-        // Actually MockPlayer plays whatever move is first in legal moves.
+        // Actually, MockPlayer plays whatever move is first in legal moves.
         
         // Let's give it a bit of time
         await Task.Delay(500);
@@ -60,7 +55,7 @@ public class ChessManagerTests {
         await Task.Delay(200); 
         
         // Game 2: should be queued
-        var (id2, white2, _) = await SetupGame(manager);
+        var (id2, white2, _) = SetupGame(manager);
         manager.StartGame(id2);
         
         await Task.Delay(500);
@@ -109,7 +104,7 @@ public class ChessManagerTests {
         Assert.True(throwingPlayer.Disposed);
         
         // Verify worker is still alive
-        var (idNext, w2, _) = await SetupGame(manager, MateInOneFen);
+        var (idNext, w2, _) = SetupGame(manager, MateInOneFen);
         manager.StartGame(idNext);
         await Task.Delay(500);
         Assert.True(w2.Disposed);
@@ -140,7 +135,7 @@ public class ChessManagerTests {
     }
 
     [Fact]
-    public async Task CleanupLoop_RemovesExpiredBuilders() {
+    public void CleanupLoop_RemovesExpiredBuilders() {
         // Arrange
         // We can't easily wait 10 minutes in a unit test, and we can't easily inject time.
         // But we can check if it compiles and runs.
@@ -154,9 +149,3 @@ public class ChessManagerTests {
     }
 }
 
-class DisposingThrowerPlayer : MockPlayer {
-    public override void Dispose() {
-        base.Dispose();
-        throw new Exception("Dispose failed");
-    }
-}
